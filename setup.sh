30a9d6b3cf6a21548c388e3cefa3fe4fc7d2a053
jraberg <jonas.raberg@gmail.com>
2018-08-25 17:04:25 +0200

    Updated configuration.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
+INST_DIR="~/tmp/dotfiles"
+
+# create base directories
 mkdir -p ~/projects
 mkdir -p ~/projects/bitbucket
 mkdir -p ~/projects/github.com
 mkdir -p ~/projects/scratchpad
+
+# ssh configuration
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+touch -p ~/.ssh/authorized
+chmod 600
+cp "$INST_DIR/ssh_config" "~/.ssh/config"
+chmod 600 "~/.ssh/config"
+
+# vim
+
+# zsh
+
+# postgres
+
+# mysql
+
+# git
