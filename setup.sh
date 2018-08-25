109b06a599676c9d23ea5e6d262669079375bd13
jraberg <jonas.raberg@gmail.com>
2018-08-25 18:41:23 +0200

    Fixed typo in setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@
 ##############################################################################
 
 
-export INST_DIR="~/tmp/dotfiles"
+export INST_DIR='~/tmp/dotfiles'
 
 #----------------------------------------------------------------------------
 # create base directories
 #----------------------------------------------------------------------------
-echo "===> Creating base directories <==="
+echo '===> Creating base directories <==='
 mkdir -p ~/projects
 mkdir -p ~/projects/bitbucket
 mkdir -p ~/projects/github.com
@@ -20,67 +20,67 @@ mkdir -p ~/projects/scratchpad
 #----------------------------------------------------------------------------
 # ssh configuration
 #----------------------------------------------------------------------------
-echo "===> Installing ssh configuration <==="
+echo '===> Installing ssh configuration <==='
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
-cp "$INST_DIR/ssh_config" "~/.ssh/config"
-chmod 600 "~/.ssh/config"
+cp '$INST_DIR/ssh_config' '~/.ssh/config'
+chmod 600 '~/.ssh/config'
 
 #----------------------------------------------------------------------------
 # aws configuration
 #----------------------------------------------------------------------------
-echo "===> Creating empty AWS cli configuration <==="
-mkdir -p "~/.aws"
-chmod 750 "~/.aws"
-cp  "$INST_DIR/aws_credentials" "~/.aws/credentials"
-chmod 600 "~/.aws/credentials"
-cp  "$INST_DIR/aws_config" "~/.aws/config"
-chmod 600 "~/.aws/config"
+echo '===> Creating empty AWS cli configuration <==='
+mkdir -p '~/.aws'
+chmod 750 '~/.aws'
+cp  '$INST_DIR/aws_credentials' '~/.aws/credentials'
+chmod 600 '~/.aws/credentials'
+cp  '$INST_DIR/aws_config' '~/.aws/config'
+chmod 600 '~/.aws/config'
 
 #----------------------------------------------------------------------------
 # vim
 #----------------------------------------------------------------------------
-echo "===> Installing vim configuration <==="
-cp "$INST_DIR/.vimrc" "~/"
-chmod 750 "~/.vimrc"
+echo '===> Installing vim configuration <==='
+cp '$INST_DIR/.vimrc' '~/'
+chmod 750 '~/.vimrc'
 
 #----------------------------------------------------------------------------
 # zsh
 #----------------------------------------------------------------------------
-echo "===> Installing zsh configuration <==="
-cp "$INST_DIR/.alias" "~/"
-cp "$INST_DIR/.functions" "~/"
-cp "$INST_DIR/.zshrc" "~/"
-chmod 750 "~/.alias"
-chmod 750 "~/.functions"
-chmod 750 "~/.zshrc"
+echo '===> Installing zsh configuration <==='
+cp '$INST_DIR/.alias' '~/'
+cp '$INST_DIR/.functions' '~/'
+cp '$INST_DIR/.zshrc' '~/'
+chmod 750 '~/.alias'
+chmod 750 '~/.functions'
+chmod 750 '~/.zshrc'
 
 #----------------------------------------------------------------------------
 # postgres
 #----------------------------------------------------------------------------
-echo "===> Installing postgres configuration <==="
-cp "$INST_DIR/.psqlrc" "~/"
-chmod 750 "~/.psqlrc"
+echo '===> Installing postgres configuration <==='
+cp '$INST_DIR/.psqlrc' '~/'
+chmod 750 '~/.psqlrc'
 
 #----------------------------------------------------------------------------
 # mysql
 #----------------------------------------------------------------------------
-echo "===> Installing mysql configuration <==="
-cp "$INST_DIR/ssh_config" "~/"
-chmod 750 "~/.ssh/config"
+echo '===> Installing mysql configuration <==='
+cp '$INST_DIR/ssh_config' '~/'
+chmod 750 '~/.ssh/config'
 
 #----------------------------------------------------------------------------
 # git
 #----------------------------------------------------------------------------
-echo "===> Installing git configuration <==="
-cp "$INST_DIR/.gitconfig" "~/"
-cp "$INST_DIR/.gitignore_global" "~/"
-cp "$INST_DIR/.*.gitignore" "~/"
-chmod 750 "~/.gitconfig"
-chmod 750 "~/.gitignore_global"
-chmod 750 "~/.*.gitignore"
+echo '===> Installing git configuration <==='
+cp '$INST_DIR/.gitconfig' '~/'
+cp '$INST_DIR/.gitignore_global' '~/'
+cp '$INST_DIR/.*.gitignore' '~/'
+chmod 750 '~/.gitconfig'
+chmod 750 '~/.gitignore_global'
+chmod 750 '~/.*.gitignore'
 
 
