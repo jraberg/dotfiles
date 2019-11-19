2838cf68bf2cd8d03c34772697a568e31e024c37
Jonas Råberg <jraberg@users.noreply.github.com>
2019-11-19 21:16:01 +0100

    Create setup.sh

---
+++ nvim/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y neovim
+mkdir ~/.config/nvim
+touch ~/.config/nvim/init.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \\nhttps://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+pip3 install pynvim
+pip3 install jedi
+pip3 install yapf
+pip3 install pylint
